d2982ce80b3ac8e606e6ba80f4ffe6146800bf9e
chaehuiseon <smilechs34@gmail.com>
2022-12-08 15:48:05 +0900

    testing junit

--- test/com/holub/MainTest.java
+++ test/com/holub/MainTest.java
@@ -1,5 +1,6 @@
 package com.holub;
 
+import com.holub.life.GridCellsize;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,15 @@ class MainTest {
     @Test
     public void add(){
 
-        assertEquals(1 + 3, 4);
+        //assertEquals(1 + 3, 4);
+        //given
+        GridCellsize testsingleton = GridCellsize.getInstance(8,8);
+        GridCellsize testsingleton2 = GridCellsize.getInstance(4,8);
+
+
+        //when
+
+
+        //then
     }
 }
