b5c3bab45ec3e131e4c4d9bace2c382b1455476b
chaehuiseon <smilechs34@gmail.com>
2022-12-08 13:58:52 +0900

    junit

---
+++ test/com/holub/MainTest.java
@@ -0,0 +1,13 @@
+package com.holub;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void add(){
+
+        assertEquals(1 + 3, 4);
+    }
+}
