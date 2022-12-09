d555b68847a06be5d14d83e6ecef8d5ab3174bc5
chaehuiseon <smilechs34@gmail.com>
2022-12-10 04:21:24 +0900

    revert add changeMapSizeTest. reason : not found main class error

--- test/com/holub/MapSizeTest.java
+++ test/com/holub/MapSizeTest.java
@@ -17,22 +17,24 @@ public class MapSizeTest {
     public void EqualSingletonInstanceTest(){
 
         //given
-        GridCellsize testsingleton = GridCellsize.getInstance();
-        GridCellsize testsingleton2 = GridCellsize.getInstance();
-
+        GridCellsize a = GridCellsize.getInstance();
+        GridCellsize b = GridCellsize.getInstance();
 
         //when
 
-
         //then
+        a.changeGridSize(4);
+        b.changeGridSize(2);
 
         //1개의 instance만 생성하므로 같아야 한다.
-        assertEquals(testsingleton,testsingleton2);
+        assertEquals(a,b);
+        System.out.println("a: " + a);
+        System.out.println("b : "+ b);
 
         //마찬가지로 1개의 instance만 생성하기 때문에, 마지막 2로 바뀐 값으로 같아야 한다.
-        assertEquals(testsingleton.getGridSize(),testsingleton2.getGridSize());
-        System.out.println(testsingleton.getGridSize());
-        System.out.println(testsingleton2.getGridSize());
+        assertEquals(a.getGridSize(),b.getGridSize());
+        System.out.println("a gridSize : "+ a.getGridSize());
+        System.out.println("b gridSize : "+b.getGridSize());
 
     }
 
@@ -56,7 +58,7 @@ public class MapSizeTest {
         //System.out.println(gridCellsize.getCellSize());
         // 따라서, 전체 cell의 개수는 map의 한변 길이 * 각 grid의 한변 길이 * 전체 grid 개수
         int totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
-        System.out.println(totalCell);
+        System.out.println("2*2: "+ totalCell);
 
         //then
         // 2*2 일 때,
@@ -69,7 +71,7 @@ public class MapSizeTest {
         gridCellsize.changeGridSize(change);
         outermostCell.changeMap2();
         totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
-        System.out.println(totalCell);
+        System.out.println("4*4: "+ totalCell);
         //then
         assertEquals(1024,totalCell);
 
@@ -79,7 +81,7 @@ public class MapSizeTest {
         gridCellsize.changeGridSize(change);
         outermostCell.changeMap2();
         totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
-        System.out.println(totalCell);
+        System.out.println("8*8: "+ totalCell);
         //then
         assertEquals(4096,totalCell);
 
