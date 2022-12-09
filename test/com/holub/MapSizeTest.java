e4b2ad23c2ef43e95d5d460546094fff30e9147f
chaehuiseon <smilechs34@gmail.com>
2022-12-10 04:01:02 +0900

    [#11] add changeMapSizeTest

--- test/com/holub/MapSizeTest.java
+++ test/com/holub/MapSizeTest.java
@@ -1,7 +1,14 @@
 package com.holub;
 
+import com.holub.life.Cell;
 import com.holub.life.GridCellsize;
+import com.holub.life.Neighborhood;
+import com.holub.life.Resident;
 import org.junit.jupiter.api.Test;
+import java.awt.geom.Rectangle2D;
+import java.beans.Transient;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,8 +22,7 @@ public class MapSizeTest {
 
 
         //when
-        testsingleton.changeGridSize(4);
-        testsingleton2.changeGridSize(2);
+
 
         //then
 
@@ -27,6 +33,57 @@ public class MapSizeTest {
         assertEquals(testsingleton.getGridSize(),testsingleton2.getGridSize());
         System.out.println(testsingleton.getGridSize());
         System.out.println(testsingleton2.getGridSize());
+
+    }
+
+    @Test
+    public void changeMapSizeTest(){
+        //given
+        GridCellsize gridCellsize = GridCellsize.getInstance();
+        int change = 2;
+        Cell outermostCell = new Neighborhood(
+                                new Neighborhood(
+                                    new Resident()
+                                )
+                             );
+
+        //when
+        gridCellsize.changeGridSize(change);
+        outermostCell.changeMap2();
+        //가장 외곽 Cell 의 갯수, 즉 , 한 변의 길이.
+        //System.out.println(outermostCell.widthInCells());
+        //각 각의 grid를 하나의 사각형이라고 했을 때, 한 변의 길이가 cell의 갯수.
+        //System.out.println(gridCellsize.getCellSize());
+        // 따라서, 전체 cell의 개수는 map의 한변 길이 * 각 grid의 한변 길이 * 전체 grid 개수
+        int totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
+        System.out.println(totalCell);
+
+        //then
+        // 2*2 일 때,
+        assertEquals(256,totalCell);
+
+
+        // 4*4 일 때,
+        //when
+        change = 4;
+        gridCellsize.changeGridSize(change);
+        outermostCell.changeMap2();
+        totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
+        System.out.println(totalCell);
+        //then
+        assertEquals(1024,totalCell);
+
+        //8*8 일 때,
+        //when
+        change = 8;
+        gridCellsize.changeGridSize(change);
+        outermostCell.changeMap2();
+        totalCell = outermostCell.widthInCells() * gridCellsize.getCellSize() * gridCellsize.getGridSize();
+        System.out.println(totalCell);
+        //then
+        assertEquals(4096,totalCell);
+
+
 
     }
 }
